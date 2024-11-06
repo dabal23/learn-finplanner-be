fb38d838f4421ae7d1dd465bedaed9ef6eb536d3
dabal23 <iqbalmalang61@gmail.com>
2024-11-07 01:46:41 +0800

    add auth controller

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -2,9 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class authController extends Controller
 {
-    //
+    public function register(Request $request){
+        $field = $request->validate([
+            'name'=>'required|max:255',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|confirmed'
+        ]);
+
+        $user = User::create($field);
+        $token = $user->createToken($request->name);
+
+        return [
+            'user'=>$user,
+            'token'=>$token->plainTextToken
+        ];
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email'=>'required|email|exists:users',
+            'password'=>'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::match ($request->password, $user->password)) {
+             return[
+                'errors' =>[
+                    'email' => ['The provided credentials are incorrect']
+                ]
+                ];
+        }
+
+        $token = $user->createToken($user->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ];
+
+    }
+
+    public function logout(Request $request){
+        $request->user()->tokens()->delete();
+
+        return ['message' => 'you are logout'];
+    }
+
 }
